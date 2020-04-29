ae40abda4c2842ced821bcbb4197425360801f75
Michał Skwara <michalon2000@gmail.com>
2020-04-29 15:02:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+
+const app = express();
+
+// 1) MIDDLEWARES
+// Security HTTP Headers
+app.use(helmet());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+// Limit request from same api
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter polution
+app.use(
+    hpp({
+        whitelist: [
+            "duration",
+            "ratingsAverage",
+            "ratingsQuantity",
+            "maxGroupSize",
+            "difficulty",
+            "price",
+        ],
+    })
+);
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middlewares
+app.use((req, res, next) => {
+    console.log("Hello from the middleware!");
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    // console.log(req.headers);
+    next();
+});
+
+// 3) ROUTES
+
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/reviews", reviewRouter);
+
+app.all("*", (req, res, next) => {
+    next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
+
+// 4) START SERVER
+
+module.exports = app;
